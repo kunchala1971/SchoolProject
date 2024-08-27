1989d22d5da35034409279e73c75381de42f1d20
kunchala1971 <kunchala1971@gmail.com>
2024-08-27 10:12:56 +0530

    afterGitIgnoreAdd

--- school/admission/views.py
+++ school/admission/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from admission.models import Admission
 # Create your views here.
 
 def home(request):
   # return HttpResponse("<H1>Hello Sample View</H1>")
-   return render(request,'index.html',{})
+   #return render(request,'index.html',{})
+  data=Admission.objects.all()
+  res=render(request,'index.html',{'data':data})
+  return res
 
 def products(request):
   # return HttpResponse("<h1>Products Page</h1>")
